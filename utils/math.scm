e1220bdfb9d1f12112aec2c32582c7bb1f9dcbaa
Sebastian Certuche <sebascertuche@gmail.com>
2025-07-21 17:44:54 -0600

    Add 1.35-1.39

--- utils/math.scm
+++ utils/math.scm
@@ -1,11 +1,12 @@
 ; num properties
 (define (++ x)
   (+ x 1))
+(define (-- x)
+  (- x 1))
 (define (even? x)
   (= (remainder x 2) 0))
 (define (divides? a b)
   (= (remainder b a) 0))
-; nth powers
 (define (square x)
   (* x x))
 (define (cube x)
@@ -14,3 +15,12 @@
   (if (= b 0)
       a
       (gcd b (remainder a b))))
+(define (average a b)
+  (/ (+ a b) 2))
+; constants
+(define pi
+  (* 2 (acos 0)))
+(define euler
+  (exp 1))
+(define golden-ratio
+  (/ (+ 1 (sqrt 5)) 2))
