fc5ea9054c6ab8a7c8f23dd00a713624f330ecfa
Sebastian Certuche <sebascertuche@gmail.com>
2025-07-22 14:21:21 -0600

    Add expmod to math and update prime util to use the miller rabin test

--- utils/math.scm
+++ utils/math.scm
@@ -35,6 +35,23 @@
 (define (square x)
  (* x x))
 
+; binary mod exponentiation from exercise 1.28
+(define (expmod base exp m)
+ (define (expmod-square-case x)
+  (if (and (= (remainder (square x) m) 1)
+           (not (or (= x 1)
+                    (= x (- m 1)))))
+   0
+   (remainder (square x) m)))
+
+ (cond
+  ((= exp 0)
+   1)
+  ((even? exp)
+   (expmod-square-case (expmod base (/ exp 2) m)))
+  (else
+   (remainder (* base (expmod base (- exp 1) m)) m))))
+
 ; conversions
 (define (deg2rad x)
  (/ (* x pi) 180))
--- utils/prime.scm
+++ utils/prime.scm
@@ -1,31 +1,40 @@
-; from exercise 1.24
-; evaluates the primality of x
 (load "utils/math.scm")
+; evaluates the primality of x
 (define (prime? x)
- (define (expmod base exp m)
-  (cond
-   ((= exp 0)
-    1)
-   ((even? exp)
-    (remainder (square (expmod base (/ exp 2) m)) m))
-   (else
-    (remainder (* base (expmod base (- exp 1) m)) m))))
+ (miller-rabin-prime? x 10))
 
- (define (fermat-prime? x times)
-  (cond
-   ((= times 0)
-    true)
-   ((< x 2)
-    false)
-   ((fermat-test x)
-    (fermat-prime? x (- times 1)))
-   (else
-    false)))
+; from exercise 1.24
+(define (miller-rabin-prime? n times)
+ (define (miller-rabin-test n)
+  (define (try-it a)
+   (= (expmod a (- n 1) n) 1))
 
+  (try-it (+ 1 (random (- n 1)))))
+
+ (cond
+  ((= times 0)
+   true)
+  ((< n 2)
+   false)
+  ((miller-rabin-test n)
+   (miller-rabin-prime? n (- times 1)))
+  (else
+   false)))
+
+; from exercise 1.24
+(define (fermat-prime? x times)
  (define (fermat-test x)
   (define (try-it a)
    (= (expmod a x x) a))
 
   (try-it (+ 1 (random (- x 1)))))
 
- (fermat-prime? x 10))
+ (cond
+  ((= times 0)
+   true)
+  ((< x 2)
+   false)
+  ((fermat-test x)
+   (fermat-prime? x (- times 1)))
+  (else
+   false)))
